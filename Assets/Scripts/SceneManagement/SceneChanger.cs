af3f6cc26d0db7d9e0e02f212db79759770f3344
Fletcher Porter <me@fletcherporter.com>
2020-11-08 17:13:11 -0800

    Made tests pass for the scene changer

---
+++ Assets/Scripts/SceneManagement/SceneChanger.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public bool LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+        WaitForLoad();
+        return true;
+    }
+
+    public bool LoadTitle()
+    {
+        return LoadScene(_titleSceneName);
+    }
+
+    public bool LoadLevel(int levelNumber)
+    {
+        if (_levelSceneNames != null)
+        {
+            return LoadScene(_levelSceneNames[levelNumber]);
+        }
+        else
+        {
+            return false;    
+        }
+    }
+
+    public bool LoadCredits()
+    {
+        return LoadScene(_creditsSceneName);
+    }
+
+    public void AddTitleScene(string titleSceneName)
+    {
+        _titleSceneName = titleSceneName;
+    }
+
+    public void AddLevelScenes(List<string> levelSceneNames)
+    {
+        if (levelSceneNames != null)
+        {
+            _levelSceneNames = new List<string>(levelSceneNames);
+        }
+        else
+        {
+            _levelSceneNames = null;    
+        }
+    }
+
+    public void AddCreditsScene(string creditsSceneName)
+    {
+        _creditsSceneName = creditsSceneName;
+    }
+
+    public bool IsLoading {
+        get { return _isLoading; }
+    }
+
+
+    private string _titleSceneName = null;
+    private List<string> _levelSceneNames = null;
+    private string _creditsSceneName = null;
+    private bool _isLoading = false;
+
+    private void WaitForLoad() 
+    {
+        _isLoading = true;
+        SceneManager.sceneLoaded += SceneLoadDone;
+    }
+
+    private void SceneLoadDone(Scene scene, LoadSceneMode mode)
+    {
+        _isLoading = false;
+    }
+
+    private bool LoadScene(string sceneName)
+    {
+        if (sceneName != null)
+        {
+            SceneManager.LoadScene(sceneName);
+            WaitForLoad();
+            Debug.Log(_levelSceneNames[0]);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Tests/PlayMode/TestSceneChanger.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class TestSceneChanger
+    {
+        private GameObject _sceneChangerGameObject;
+        private SceneChanger _sceneChanger;
+
+        private string _titleScene = "Title";
+        private string _levelScene = "testLevel";
+        private string _creditsScene = "Credits";
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sceneChangerGameObject =
+                new GameObject("Scene Changer");
+            _sceneChangerGameObject.AddComponent<SceneChanger>();
+            _sceneChanger =
+                _sceneChangerGameObject.GetComponent<SceneChanger>();
+            _sceneChanger.AddTitleScene(_titleScene);
+            _sceneChanger.AddLevelScenes(
+                new List<string>(new string[] {_levelScene}));
+            _sceneChanger.AddCreditsScene(_creditsScene);
+
+            SceneManager.LoadScene("DummyScene");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.Destroy(_sceneChangerGameObject);
+            Object.Destroy(_sceneChanger);
+        }
+
+        [UnityTest]
+        public IEnumerator TestInitiallyLoadsToTitle()
+        {
+            Assert.That(_sceneChanger.LoadFirstScene(), Is.True);
+            while (_sceneChanger.IsLoading)
+            {
+                yield return null;
+            }
+            Scene currentScene = SceneManager.GetActiveScene();
+            Assert.That(currentScene.name, Is.EqualTo("Title"),
+                        "Fails to initially load to title.");
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TestLoadsTitle()
+        {
+            Assert.That(_sceneChanger.LoadTitle(), Is.True);
+            while (_sceneChanger.IsLoading)
+            {
+                yield return null;
+            }
+            Scene currentScene = SceneManager.GetActiveScene();
+            Assert.That(currentScene.name, Is.EqualTo("Title"),
+                        "Fails to load title screen.");
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TestLoadsLevel()
+        {
+            Assert.That(_sceneChanger.LoadLevel(0), Is.True);
+            while (_sceneChanger.IsLoading)
+            {
+                yield return null;
+            }
+            Scene currentScene = SceneManager.GetActiveScene();
+            Assert.That(currentScene.name, Is.EqualTo("testLevel"),
+                        "Fails to load the level.");
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TestLoadsCredits()
+        {
+            Assert.That(_sceneChanger.LoadCredits(), Is.True);
+            while (_sceneChanger.IsLoading)
+            {
+                yield return null;
+            }
+            Scene currentScene = SceneManager.GetActiveScene();
+            Assert.That(currentScene.name, Is.EqualTo("Credits"),
+                        "Fails to load the credits.");
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TestTitleDoesntLoadWhenNotSet()
+        {
+            _sceneChanger.AddTitleScene(null);
+            Assert.That(_sceneChanger.LoadTitle(), Is.False);
+            while (_sceneChanger.IsLoading)
+            {
+                yield return null;
+            }
+            Scene currentScene = SceneManager.GetActiveScene();
+            Assert.That(currentScene.name, Is.EqualTo("DummyScene"),
+                        "Fails to do nothing when there isn't a title "
+                        + "scene set");
+        }
+
+        [UnityTest]
+        public IEnumerator TestCreditsDontLoadWhenNotSet()
+        {
+            _sceneChanger.AddCreditsScene(null);
+            Assert.That(_sceneChanger.LoadCredits(), Is.False);
+            while (_sceneChanger.IsLoading)
+            {
+                yield return null;
+            }
+            Scene currentScene = SceneManager.GetActiveScene();
+            Assert.That(currentScene.name, Is.EqualTo("DummyScene"),
+                        "Fails to do nothing when there isn't a "
+                        + "credits scene set");
+        }
+
+        [UnityTest]
+        public IEnumerator TestLevelDoesntLoadWhenNotSet()
+        {
+            _sceneChanger.AddLevelScenes(null);
+            Assert.That(_sceneChanger.LoadLevel(0), Is.False);
+            while (_sceneChanger.IsLoading)
+            {
+                yield return null;
+            }
+            Scene currentScene = SceneManager.GetActiveScene();
+            Assert.That(currentScene.name, Is.EqualTo("DummyScene"),
+                        "Fails to do nothing when there isn't a level "
+                        + "scene set");
+        }
+    }
+}
