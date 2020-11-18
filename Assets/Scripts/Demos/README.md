c6d2d54c9c7dcf1c8dbaa2c64660f0ea86fb2e8d
Fletcher Porter <me@fletcherporter.com>
2020-11-17 21:19:57 -0800

    moved ShootMissileOnClick to new Demos folder

---
+++ Assets/Scripts/Demos/README.md
@@ -0,0 +1,3 @@
+# Demoes
+
+These are quick scripts to demonstrate how in-game stuff works.  They are not meant to be used in the final game.
--- Assets/Scripts/Demos/ShootMissileOnClick.cs
+++ Assets/Scripts/Demos/ShootMissileOnClick.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class ShootMissileOnClick : MonoBehaviour
 {
     public GameObject missile;
-    public Camera camera;
+
+
+    private Camera _camera;
 
     private void Update()
     {
@@ -12,10 +14,15 @@ public class ShootMissileOnClick : MonoBehaviour
         {
             GameObject newMissile = Instantiate(
                 missile,
-                camera.ScreenToWorldPoint(Input.mousePosition)
-                    - camera.transform.position.z*Vector3.forward,
+                _camera.ScreenToWorldPoint(Input.mousePosition)
+                    - _camera.transform.position.z*Vector3.forward,
                 Quaternion.identity);
             newMissile.GetComponent<Rigidbody2D>().velocity = 4.0f*Vector2.up;
         }
+    }
+
+    private void Awake()
+    {
+        _camera = Camera.main;
     }
 }
